2fa8bbe5a644fc47153d9f4e37a5f6d037eaf37f
Mario Naranjo <marionaranjo12@gmail.com>
2015-10-19 16:05:24 -0600

    Versión 1

--- needleman_wunsch.h
+++ needleman_wunsch.h
@@ -5,6 +5,8 @@ typedef aligner_t nw_aligner_t;
 
 //MÉTODOS ABSTRACTOS PARA EL ALINEAMIENTO
 nw_aligner_t* needleman_wunsch_new();
-void needle_wunsh_free(nw_aligner *nw);
+void needleman_wunsh_free(nw_aligner *nw);
+void needleman_wunsh_align(const char *a, const char *b,
+                           const scoring_t *scoring);
 
 #endif // NEEDLEMAN_WUNSCH_H_INCLUDED
