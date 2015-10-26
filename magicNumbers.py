b3f8361ddec039f3fb5fcd428816c8ea047c7a54
rlip1994 <solisbarrantes@gmail.com>
2015-10-26 17:51:19 -0600

    world
    autoo genera el mundo

--- magicNumbers.py
+++ magicNumbers.py
@@ -1,10 +1,10 @@
-world = {"0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"A":10,"B":11,"C":12,"D":13,"E":14,"F":15,"G":16,"H":17,"I":18,"J":19,"K":20,"L":21,"M":22,"N":23,"O":24,"P":25,"Q":26,"R":27,"S":28,"T":29,"U":30,"V":31,"W":32,"X":33,"Y":34,"Z":35,"*":26}
+#world = {"0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"A":10,"B":11,"C":12,"D":13,"E":14,"F":15,"G":16,"H":17,"I":18,"J":19,"K":20,"L":21,"M":22,"N":23,"O":24,"P":25,"Q":26,"R":27,"S":28,"T":29,"U":30,"V":31,"W":32,"X":33,"Y":34,"Z":35,"*":26}
 
 
 def main():
-    l=makelist("24F")
-    l.reverse()
-    print(doMagic2(l,[],0,16,10))
+    l=(genWorld(100).items())
+    l.sort()
+    print(l)
 
 
 
@@ -21,6 +21,29 @@ def doMagic2(list1,list2,cont,bo,bd):
     numRed=(world[list1[0][0]]+(26*cuentaPalo(list1[0])))*x
     list2.append(numRed)
     return doMagic2(list1[1:],list2,cont+1,bo,bd)
+
+
+def genWorld(base):
+    world={}
+    i=0
+    j=1
+    h=1
+    while i<base:
+        if i<10:
+            world[str(chr(48+i))]=i
+        else:
+            if j % 27 == 0:
+                h+=1
+                j=1
+            for x in range(h):
+                world[str(chr(65+j-1))+("*"*(h-1))]=i
+            j+=1
+        
+        i+=1
+    return world
+        
+        
+        
 
 def cuentaPalo(string):
     x=0
