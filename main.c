816a0e7bb817523b88291c049d9270808c1204a8
Mario Naranjo <mario15695@hotmail.com>
2015-11-02 10:40:19 -0600

    ReadFile and NW

--- main.c
+++ main.c
@@ -4,106 +4,29 @@
 #include <string.h>
 #include <limits.h>
 #include <ctype.h>
-//#include "align.h"
 
 
+#include "readFile.c"
 #include "alineamientos/needleman_wunsch.c"
 
 //NEEDLEMAN WUNSCH ALGORITHM
 
 int main() {
-    char str1[] = "GCATGCU";
-    char str2[] = "GATTACA";
 
-    needleman_wunsch(str1, str2);
-
-    return 0;
-
-/*
-    //Tamaños de las hileras (secuencias)
-    char str1[] = "ATTGTGATCC";
-    char str2[] = "TTGCATCGGC";
-    int dimString1 = sizeof(str1)/sizeof(str1[0]);
-    int dimString2 = sizeof(str2)/sizeof(str2[0]);
-    //Gap penalty, match, mismatch
-    int GAP_PENALTY = -2;
-    int MATCH = 1;
-    int MISMATCH = -1;
-
-    int table[dimString1 + 1][dimString2 + 1], i, j;
-
-    //Iniciar la matriz para las hileras
-    for (i = 0; i <= dimString1; i ++)
-    {
-        table[i][0] = i * GAP_PENALTY;
-    }
-    for (j = 0; j <= dimString2; j ++)
-    {
-        table[0][j] = j * GAP_PENALTY;
-    }
-
-    //Algoritmo Needleman-Wunsch
-    int s,a,b,c;
-
-    for (i = 1; i <= dimString1; i ++)
-    {
-        for (j = 1; j <= dimString2; j ++)
-        {
-            if (str1[j - 1] == str2[i - 1])
-            {
-                s = MATCH;
-            }
-            else
-            {
-                s = MISMATCH;
-            }
-
-            a = table[i - 1][j - 1] + s;
-            b = table[i][j - 1] + GAP_PENALTY;
-            c = table[i - 1][j] + GAP_PENALTY;
-
-            table[i][j] = maximum(a, b, c);
-        }
-    }
-    printf("%i", table[10][10]);
-*/
-
-    /*
-    char *sequence1, *sequence2;  //store sequences returned by loadSequence
-    int gapInit, gapXtnd;         //store gap penalty factors
-    Alignment result;             //store result obtained from align
-
+    char *sequence1, *sequence2;
     sequence1 = loadSequence("hola1.seq");
     sequence2 = loadSequence("hola2.seq");
 
-    if(!(sequence1 && sequence2))
-    {
-        fprintf(stderr, "Error reading sequences\n");
+    if(!(sequence1 && sequence2)) {
+        fprintf(stderr, "Error al leer las secuencias\n");
         exit(EXIT_FAILURE);
     }
 
-    gapInit = 0;
-    gapXtnd = 0;
-
-    if(gapInit < 0)
-    {
-        fprintf(stderr, "%i is invalid gap opening penalty\n", gapInit);
-        exit(EXIT_FAILURE);
-    }
-
-    if(gapXtnd < 0)
-    {
-        fprintf(stderr, "%i is invalid gap elongation factor\n", gapXtnd);
-        exit(EXIT_FAILURE);
-    }
-
-    result = align(sequence1, sequence2, gapInit, gapXtnd);
+    needleman_wunsch(sequence1, sequence2);
 
     free(sequence1);
     free(sequence2);
 
-    printAlignment(result, 79);
+    return 0;
 
-    free(result.first);
-    free(result.second);*/
 }
---
+++ readFile.c
@@ -0,0 +1,48 @@
+#ifndef READFILE_C_INCLUDED
+#define READFILE_C_INCLUDED
+
+/*loadSequence takes a filename, loads all the alphabetic characters in the
+  file into a string (converting to upper case) and returns the string, or NULL
+  on error*/
+char *loadSequence(char *filename) {
+  FILE *seqFile;    /*pointer to sequence file*/
+  char *seqString;  /*sequence string*/
+  int i = 0;        /*iterator*/
+  int fileLength;   /*store length of file--so can malloc string once*/
+  int c;            /*hold current character off of file stream*/
+
+  if(!(seqFile = fopen(filename, "r"))) { /*if file is unopenable, exit*/
+    fprintf(stderr, "Cannot open file: %s\n", filename);
+    return(NULL);
+  }
+
+  fseek(seqFile, 0, SEEK_END);        /*move file pointer to end of file*/
+  fileLength = (int) ftell(seqFile);  /*store offset from beginning (size)*/
+
+  if(!fileLength) {                   /*file is of length zero*/
+    fclose(seqFile);
+    fprintf(stderr, "File %s contains no data\n", filename);
+    return(NULL);
+  }
+
+  if(fileLength == INT_MAX) {          /*file too long for this simple program*/
+    fclose(seqFile);
+    fprintf(stderr, "Sequence in %s is too long for this program!\n", filename);
+    return(NULL);
+  }
+
+  seqString = (char *) malloc((fileLength + 1) * sizeof(char)); /*roomfor '\0'*/
+  rewind(seqFile);                    /*go back to beginning to read file*/
+
+  while((c = fgetc(seqFile)) != EOF)   /*read until end-of-file*/
+    if(isalpha(c))
+      seqString[i++] = (char) toupper(c);
+
+  fclose(seqFile);
+
+  seqString[i] = '\0';                /*null-terminate string*/
+  return(seqString);
+}
+
+
+#endif // READFILE_C_INCLUDED
